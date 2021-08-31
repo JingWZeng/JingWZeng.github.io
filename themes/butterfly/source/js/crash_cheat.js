4127400bfa03d7e81a79d0aac00db858cdb40ff9
zeng <3117005065@mail2.gdut.edu.cn>
2021-08-31 21:30:43 +0800

    2021/8/31

---
+++ themes/butterfly/source/js/crash_cheat.js
@@ -0,0 +1,20 @@
+//崩溃欺骗 + OriginTitle 
+ var OriginTitle = document.title;
+ var titleTime;
+
+btf.isJqueryLoad(function(){
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            $('[rel="icon"]').attr('href', "/LightCycle.ico");
+            document.title = '╭(°A°`)╮ 客官不要走嘛~';
+            clearTimeout(titleTime);
+        }
+        else {
+            $('[rel="icon"]').attr('href', "https://raw.githubusercontent.com/JingWZeng/markdownImg/main/img/202108232012733.png");
+            document.title = '(ฅ>ω<*ฅ) 客官欢迎回来~';
+            titleTime = setTimeout(function () {
+                document.title = OriginTitle;
+            }, 2000);
+        }
+    });
+})
