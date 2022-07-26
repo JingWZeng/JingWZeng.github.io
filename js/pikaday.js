fd9dde0aa285ab06ce220e4a72d5373744c64459
JingWZeng <88088364+JingWZeng@users.noreply.github.com>
2022-07-26 06:19:19 +0000

    Deploying to master from  @ 02d6c4ff443d0435db4c169094f4fbe30edb3c56 🚀

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2021":{"7":{"20":true,"24":true,"25":true,"26":true,"27":true,"31":true},"8":{"2":true,"15":true,"24":true,"26":true,"27":true,"30":true},"9":{"12":true,"13":true,"14":true,"20":true,"21":true,"25":true,"26":true,"28":true,"30":true},"10":{"12":true,"15":true,"22":true,"25":true,"29":true,"30":true},"11":{"2":true,"7":true,"10":true}},"2022":{"0":{"4":true,"19":true}}}';
+      var data = '{"2021":{"7":{"20":true,"24":true,"25":true,"26":true,"27":true,"31":true},"8":{"2":true,"15":true,"24":true,"26":true,"27":true,"30":true},"9":{"12":true,"13":true,"14":true,"20":true,"21":true,"25":true,"26":true,"28":true,"30":true},"10":{"12":true,"15":true,"22":true,"25":true,"29":true,"30":true},"11":{"2":true,"7":true,"10":true}},"2022":{"0":{"4":true,"19":true},"6":{"26":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
